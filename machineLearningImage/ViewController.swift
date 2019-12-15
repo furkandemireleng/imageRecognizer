9e4ac6d97de7d6748b1efd18ac80efd6d85dbf56
Furkan Demirel <furkandemireleng@gmail.com>
2019-12-15 22:12:45 +0300

    Initial Commit

---
+++ machineLearningImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  machineLearningImage
+//
+//  Created by MacxbookPro on 15.12.2019.
+//  Copyright © 2019 MacxbookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
